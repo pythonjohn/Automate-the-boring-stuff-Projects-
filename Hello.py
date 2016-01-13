8a43065028f647d1caa3818d218295a8ba27afec
Python_John <pythonjohn@users.noreply.github.com>
2016-01-13 09:53:29 -0500

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,9 @@
+print ('Hello world!')
+print ('What is your name?') # ask for thier name
+myName = input()
+print ('It is good to meet you, ' + myName)
+print ('The length of your name is:')
+print (len(myName))
+print ('What is your age') # Ask for thier age
+myAge = input()
+print ('You will be ' +str(int(myAge) +1) + ' in a year')
